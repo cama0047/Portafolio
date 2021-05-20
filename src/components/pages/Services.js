f46e1bcfe7da4f0ac4e5fed750187ab47651fa17
Camilo CV <cama0047@algonquinlive.com>
2021-05-20 01:37:13 -0400

    AboutMe -> expandir funcionando

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -7,66 +7,60 @@ export default function Services() {
   
   const [click, setClick] = useState(false);
   const [isActive, setActive] = useState(false);
-  const [expanded, setExpanded] = useState(true);
-  
-  const handleClick = (ev) => {
-    
-      /*------ PRINT -------*/
-      //console.log("clicked");
-      //console.log(ev.target);
-      const divClicked=ev.target;
+  const [expanded, setExpanded] = useState(false);
+  const id ="a";
 
-      setExpanded(true);
-      open(divClicked);
+
+
+  const handleClick = (ev) => {
+   
+    console.log(ev.target.id);
+
+    const divClicked = ev.target.id; //identificando que elemento fue clickeado
+    expandir(divClicked); //se llama a la funcion expanir y por parametro el objeto clickeado
+    
 
   }
+
+  const expandir = (divClicked) =>{
 
   var tile = document.getElementsByClassName('.strips__strip');
   var tileLink = document.getElementsByClassName('.strips__strip > .strip__content');
-  var tileText = document.getElementsByClassName('.strip__inner-text');
+  //var tileText = tileLink.document.getElementsByClassName('.strip__inner-text');
   var stripClose = document.getElementsByClassName('.strip__close');
-  
-  var open = function(divClicked) {
-      console.log("Entro a Open");
-      console.log(expanded);
 
-    // var tile = this.parent();
+  /*------ PRINT -------*/
+  console.log(expanded);
+  console.log("DivClicked: ",divClicked); //imprimir que fue clickeado 
 
-      if (expanded) {
-
-        divClicked.addClass('strips__strip--expanded');
-        // add delay to inner text
-        tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
-        stripClose.addClass('strip__close--show');
-        stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
-        // expanded = false;
-      } 
-    };
-  
-  var close = function() {
     if (!expanded) {
-      tile.removeClassName('strips__strip--expanded');
-      // remove delay from inner text
-      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
-      stripClose.removeClassName('strip__close--show');
-      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
-      expanded = false;
+      document.getElementById(`${divClicked}`).classList.add("strips__strip--expanded");
+      
+      setExpanded(true);
+    }
+
+    if (expanded) {
+      document.getElementById(`${divClicked}`).classList.remove("strips__strip--expanded");
+      setExpanded(false);
     }
   }
 
-
+  
   return (
 
 <div>
   <h1>About Me</h1>
-  <p>
+  <div className="me">
     Experienced Full Stack Developer with 10 years in the industry and high-level proficiency in multiple technologies, expertise with Front and back-end including DevOps seeks to establish a career as the Full Stack Developer. I am very familiar and experienced with Web Application Architecture. Furthermore, I have led several development teams with successful results.
-  </p>
+    
+      <p><b>Please Click the boxes to have more details.</b></p>
+  </div>
 
   <div>
     <section className="strips">
-  <article className="strips__strip">
-    <div className={`strip__content ${isActive ? "strips__strip--expanded":"true"}`} onClick={()=>setActive(!isActive)}>
+
+  <article id="back" className="strips__strip" onClick={handleClick}>
+    <div  id="back" className="strip__content">
       <h1 className="strip__title" data-name="Back">Back-End</h1>
       <div className="strip__inner-text">
         <h2>Back-End</h2>
@@ -74,12 +68,14 @@ export default function Services() {
         <p>
           <Link href="https://twitter.com/ettrics" target="_blank"><i className="fa fa-twitter"></i></Link>
         </p>
+    
       </div>
       
     </div>
   </article>
-  <article className="strips__strip">
-    <div className={`strip__content ${isActive ? "strips__strip--expanded":"true"}`} onClick={()=>setActive(!isActive)}>
+
+  <article id="front" className="strips__strip" onClick={handleClick}>
+    <div id="front" className="strip__content">
       <h1 className="strip__title" data-name="Front">Front-End</h1>
       <div className="strip__inner-text">
         <h2>Front-End</h2>
@@ -90,8 +86,8 @@ export default function Services() {
       </div>
     </div>
   </article>
-  <article className="strips__strip">
-        <div className={`strip__content ${isActive ? "strips__strip--expanded":"true"}`} onClick={()=>setActive(!isActive)}>
+  <article id="database" className="strips__strip" onClick={handleClick}>
+        <div id="database" className="strip__content">
       <h1 className="strip__title" data-name="Data">Databases</h1>
       <div className="strip__inner-text">
         <h2>Databases</h2>
@@ -102,8 +98,8 @@ export default function Services() {
       </div>
     </div>
   </article>
-  <article className="strips__strip">
-        <div className={`strip__content ${isActive ? "strips__strip--expanded":"true"}`} onClick={()=>setActive(!isActive)}>
+  <article id="devops" className="strips__strip" onClick={handleClick}>
+        <div id="devops" className="strip__content">
       <h1 className="strip__title" data-name="Dev">DevOps</h1>
       <div className="strip__inner-text">
         <h2>DevOps</h2>
@@ -114,8 +110,8 @@ export default function Services() {
       </div>
     </div>
   </article>
-  <article className="strips__strip">
-        <div className={`strip__content ${isActive ? "strips__strip--expanded":"true"}`} onClick={()=>setActive(!isActive)}>
+  <article id="mobile" className="strips__strip" onClick={handleClick}>
+        <div id="mobile" className="strip__content">
       <h1 className="strip__title" data-name="Movile">MobileApps</h1>
       <div className="strip__inner-text">
         <h2>MobileApps</h2>
