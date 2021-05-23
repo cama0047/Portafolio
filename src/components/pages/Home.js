f33de5fa0eb47324e244989fa3cc6d6cb31b637e
Camilo CV <cama0047@algonquinlive.com>
2021-05-22 23:06:51 -0400

    arreglando links 2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-import CardGrid from '../CardGrid';
+
 
 function Home() {
   return (
